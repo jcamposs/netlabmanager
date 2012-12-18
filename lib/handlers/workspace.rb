e8047aaca9ec84c2331cd781447c09b7228dd6f1
Santiago Carot Nemesio <sancane@gmail.com>
2012-12-18 12:26:32 +0100

    Initialize workspace service handler

--- lib/handlers/workspace.rb
+++ lib/handlers/workspace.rb
@@ -1,4 +1,10 @@
 class Workspace < NetlabManager::ServiceHandler
+  def initialize
+    @chan = AMQP::Channel.new
+    @queue = @chan.queue("netlab.services.workspace.state", :exclusive => true, :auto_delete => true)
+    @initialized = true
+  end
+
   def start
     #TODO: Implement this
   end
