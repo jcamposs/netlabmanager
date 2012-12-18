3c0db95e29332a4f6513758695ed0d365ca3e7c4
Santiago Carot Nemesio <sancane@gmail.com>
2012-12-18 12:27:33 +0100

    Implement main operations for workspace handler

--- lib/handlers/workspace.rb
+++ lib/handlers/workspace.rb
@@ -6,10 +6,55 @@ class Workspace < NetlabManager::ServiceHandler
   end
 
   def start
-    #TODO: Implement this
+    return false if not @initialized
+
+    @queue.subscribe(:ack => true) do |metadata, payload|
+      DaemonKit.logger.debug "[requests] Workspace id #{payload}."
+      EventMachine.synchrony do
+        reply = get_workspace payload
+
+        @chan.default_exchange.publish(reply,
+          :routing_key => metadata.reply_to,
+          :correlation_id => metadata.message_id,
+          :content_type => "application/json",
+          :mandatory => true)
+        metadata.ack
+      end
+    end
+
+    return true
   end
 
   def stop
-    #TODO: Implement this
+    return if not @initialized
+    @queue.delete
+    @chan.close
+    @initialized = false
+  end
+
+  private
+  def compose_json_reply(nodes, id)
+    obj = {
+      "workspace" => id,
+      "nodes" => []
+    }
+
+    nodes.keys.each do |name|
+      obj["nodes"].push({
+        "name" => name,
+        "state" => nodes[name]
+      })
+    end
+
+    return obj.to_json
+  end
+
+  def get_workspace id
+    nodes = {}
+    #VirtualMachine.find_all_by_workspace_id(id).each do |vm|
+    #  nodes[vm.name] = vm.state
+    #end
+
+    compose_json_reply(nodes, id)
   end
 end
