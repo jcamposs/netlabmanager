5ceeac2bfec30e964b4ddaebe4b543a306b27f81
Santiago Carot Nemesio <sancane@gmail.com>
2012-12-19 10:46:07 +0100

    Set handler inside NetlabHandler namespace to avoid name collisions

--- lib/handlers/workspace.rb
+++ lib/handlers/workspace.rb
@@ -1,60 +1,62 @@
-class Workspace < NetlabManager::ServiceHandler
-  def start
-    @chan = AMQP::Channel.new
-    @queue = @chan.queue("netlab.services.workspace.state", :exclusive => true, :auto_delete => true)
-    @running = true
+module NetlabHandler
+  class Workspace < NetlabManager::ServiceHandler
+    def start
+      @chan = AMQP::Channel.new
+      @queue = @chan.queue("netlab.services.workspace.state", :exclusive => true, :auto_delete => true)
+      @running = true
 
-    @queue.subscribe(:ack => true) do |metadata, payload|
-      DaemonKit.logger.debug "[requests] Workspace id #{payload}."
-      EventMachine.synchrony do
-        reply = get_workspace payload
+      @queue.subscribe(:ack => true) do |metadata, payload|
+        DaemonKit.logger.debug "[requests] Workspace id #{payload}."
+        EventMachine.synchrony do
+          reply = get_workspace payload
 
-        @chan.default_exchange.publish(reply,
-          :routing_key => metadata.reply_to,
-          :correlation_id => metadata.message_id,
-          :content_type => "application/json",
-          :mandatory => true)
-        metadata.ack
+          @chan.default_exchange.publish(reply,
+            :routing_key => metadata.reply_to,
+            :correlation_id => metadata.message_id,
+            :content_type => "application/json",
+            :mandatory => true)
+          metadata.ack
+        end
       end
+
+      return true
     end
 
-    return true
-  end
+    def stop
+      if not @running
+        DaemonKit.logger.error "Error: Workspace service is not started"
+        return false
+      end
 
-  def stop
-    if not @running
-      DaemonKit.logger.error "Error: Workspace service is not started"
-      return false
+      @queue.delete
+      @chan.close
+      @running = false
     end
 
-    @queue.delete
-    @chan.close
-    @running = false
-  end
+    private
+    def compose_json_reply(nodes, id)
+      obj = {
+        "workspace" => id,
+        "nodes" => []
+      }
 
-  private
-  def compose_json_reply(nodes, id)
-    obj = {
-      "workspace" => id,
-      "nodes" => []
-    }
+      nodes.keys.each do |name|
+        obj["nodes"].push({
+          "name" => name,
+          "state" => nodes[name]
+        })
+      end
 
-    nodes.keys.each do |name|
-      obj["nodes"].push({
-        "name" => name,
-        "state" => nodes[name]
-      })
+      return obj.to_json
     end
 
-    return obj.to_json
-  end
+    def get_workspace id
+      nodes = {}
+      VirtualMachine.find_all_by_workspace_id(id).each do |vm|
+        nodes[vm.name] = vm.state
+      end
 
-  def get_workspace id
-    nodes = {}
-    VirtualMachine.find_all_by_workspace_id(id).each do |vm|
-      nodes[vm.name] = vm.state
+      compose_json_reply(nodes, id)
     end
-
-    compose_json_reply(nodes, id)
   end
 end
