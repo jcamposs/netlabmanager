883a17b8a76b680949f5ad9225e1e662b974d877
Santiago Carot Nemesio <sancane@gmail.com>
2013-01-18 15:08:42 +0100

    Shellinabox plugin skelleton

---
+++ lib/handlers/shellinabox.rb
@@ -0,0 +1,12 @@
+module NetlabHandler
+  class Shellinabox < NetlabManager::ServiceHandler
+    def start
+      #TODO:
+      return true
+    end
+
+    def stop
+      #TODO:
+    end
+  end
+end
