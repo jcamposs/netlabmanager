752e0657b600a620a4c63ecc2bd0d3baddc7918e
Santiago Carot Nemesio <sancane@gmail.com>
2013-01-18 15:26:45 +0100

    Shutdown shellinabox plugin

--- lib/handlers/shellinabox.rb
+++ lib/handlers/shellinabox.rb
@@ -2,16 +2,27 @@ module NetlabHandler
   class Shellinabox < NetlabManager::ServiceHandler
     def start
       init_amqp_stuff
+      @running = true
       return true
     end
 
     def stop
-      #TODO:
+      shutdown_amqp_stuff
     end
 
     private
     def init_amqp_stuff
       init_started_queue
+    end
+
+    def shutdown_amqp_stuff
+      if not @running
+        DaemonKit.logger.error "Error: Workspace service is not started"
+        return
+      end
+
+      shutdown_started_queue
+      @running = false
     end
 
     def init_started_queue
@@ -30,6 +41,10 @@ module NetlabHandler
           #Todo:
         end
       end
+    end
+
+    def shutdown_started_queue
+      @start_chan.close
     end
   end
 end
